b674ce54f2030dad5a808bfb98994d67f584d067
juanPatinoo <juanpaty04@gmail.com>
2025-06-09 13:04:39 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -23,7 +23,10 @@ function mostrarNodo(id) {
 
     const msg = document.createElement('div');
     msg.className = 'message bot';
-    msg.innerHTML = `<img src="logo.png" class="avatar" alt="logo"> <div>${nodo.mensaje}</div>`;
+    msg.innerHTML = `
+      <img src="logo.png" class="avatar" alt="logo">
+      <div class="bubble">${nodo.mensaje}</div>
+    `;
     chatbox.appendChild(msg);
     chatbox.scrollTop = chatbox.scrollHeight;
 
@@ -38,11 +41,10 @@ function mostrarNodo(id) {
         
         const userMsg = document.createElement('div');
         userMsg.className = 'message user';
-        userMsg.textContent = opcion.texto;
+        userMsg.innerHTML = `<div class="bubble-user">${opcion.texto}</div>`;
         chatbox.appendChild(userMsg);
         chatbox.scrollTop = chatbox.scrollHeight;
 
-        
         buttonsContainer.remove();
 
         
@@ -57,3 +59,44 @@ function mostrarNodo(id) {
 
   }, 800);
 }
+
+let chatAbierto = false;
+const boton = document.getElementById('chatbotToggle');
+const popup = document.getElementById('chatbotPopup');
+const btnContent = boton.querySelector('.btn-content');
+
+function toggleChatbot() {
+  btnContent.style.opacity = '0';
+
+  setTimeout(() => {
+    if (!chatAbierto) {
+      popup.style.visibility = 'visible';
+      popup.classList.remove('animar-cerrar');
+      popup.classList.add('animar-abrir')
+      btnContent.innerHTML = '<img src="flechaAbajo.png" alt="Cerrar">';
+      chatAbierto = true;
+    } else {
+      popup.classList.remove('animar-abrir');
+      popup.classList.add('animar-cerrar');
+
+      btnContent.innerHTML = '<img src="chat-bot.png" alt="Icono" class="icono-chat"> ChatBot';
+      chatAbierto = false;
+
+      
+      setTimeout(() => {
+        popup.style.visibility = 'hidden';
+        popup.classList.remove('animar-cerrar');
+      }, 200);
+    }
+
+   
+    btnContent.style.opacity = '1';
+  }, 200);
+}
+
+  document.getElementById('resetButton').addEventListener('click', () => {
+  const chatbox = document.getElementById('chatbox');
+  chatbox.innerHTML = '';      
+  nodoActual = "inicio";       
+  mostrarNodo(nodoActual);
+});
